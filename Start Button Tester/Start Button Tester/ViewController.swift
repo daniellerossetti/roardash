8d57eb70819867da50575a6f0a05bf46e9e81063
rowanmacy <gwc@Girls-Who-Code-5.local>
2016-08-03 13:08:38 -0400

    Start button!!!

---
+++ Start Button Tester/Start Button Tester/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Start Button Tester
+//
+//  Created by Girls Who Code on 8/3/16.
+//  Copyright © 2016 Girls Who Code. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func ButtonClicked(sender: AnyObject)
+    {
+    
+    
+    print("The button was clicked")
+        
+    
+    
+    
+    
+    }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
