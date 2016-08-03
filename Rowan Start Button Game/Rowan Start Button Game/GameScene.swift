49fe16c9564fb0a3a9fc708bd6b3c84a766bfe1d
rowanmacy <gwc@Girls-Who-Code-5.local>
2016-08-03 14:04:58 -0400

    Clarifying comments courtesy of Rowan

--- Rowan Start Button Game/Rowan Start Button Game/GameScene.swift
+++ Rowan Start Button Game/Rowan Start Button Game/GameScene.swift
@@ -10,25 +10,31 @@ import SpriteKit
 
 class GameScene: SKScene {
 
+    // All functions below recognize startButton
     let startButton = SKLabelNode()
     
+    // Automatically show startButton when screen moves to view
     override func didMoveToView(view: SKView) {
         /* Setup your scene here */
+        // Parameters to determine visual display
         startButton.text = "Start"
         startButton.position = CGPoint(x: size.width * 0.5, y: size.height * 0.3)
 
+        // Make the startButton show up
         addChild(startButton)
         
-        let itemHide = SKAction.removeFromParent()
     }
     
+    // Runs when the screen is tapped anywhere
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-        print("click")
+        // Remove the startButton
         startButton.runAction(SKAction.removeFromParent())
         
-//        super.touchesBegan(touches, withEvent: event)
+        // Not sure what this does
+        super.touchesBegan(touches, withEvent: event)
     }
     
+    // Used for animations
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
