85e9b0e3759026b4192d13ac29f615d9ff9e019d
rowanmacy <gwc@Girls-Who-Code-5.local>
2016-08-04 13:26:19 -0400

    Comments on OneHourWalker

--- OneHourWalker-master/OneHourWalker/TimerViewController.swift
+++ OneHourWalker-master/OneHourWalker/TimerViewController.swift
@@ -5,23 +5,28 @@
 //  Created by Matthew Maher on 2/18/16.
 //  Copyright © 2016 Matt Maher. All rights reserved.
 //
+// This file has time, distance, and sharing capabilities. The share and height features that use HealthKit have been diabled. When run on the computer, it simulates a person walking at a constant pace. Pressing go after stop has been used clears the timer (no pause).
 
 import UIKit
 import CoreLocation
 //import HealthKit
 
 class TimerViewController: UIViewController, CLLocationManagerDelegate {
-
+    // References that can be reset, or mutable variables (I know I'm gettin gthe terminology wrong)
+    // Sets up the text to display for time and distance
     @IBOutlet weak var timerLabel: UILabel!
     @IBOutlet weak var milesLabel: UILabel!
     @IBOutlet weak var heightLabel: UILabel!
     
+    // zeroTime is a time interval that will be set to the time at which start is pressed. The timer is declared here
     var zeroTime = NSTimeInterval()
     var timer : NSTimer = NSTimer()
     
+    // This project uses locationManager to track GPS location and distance
     let locationManager = CLLocationManager()
     var startLocation: CLLocation!
     var lastLocation: CLLocation!
+    // Give variables starting files when start is called
     var distanceTraveled = 0.0
     var timerStartDate: NSDate!
     
@@ -31,12 +36,15 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        // Pop-up request for user to allow locatin services
         locationManager.requestWhenInUseAuthorization()
         
         if CLLocationManager.locationServicesEnabled(){
+            // If user allows location services, call the locationManager to deliver location
             locationManager.delegate = self
             locationManager.desiredAccuracy = kCLLocationAccuracyBest
         } else {
+            // An error message
             print("Need to Enable Location")
         }
         
@@ -44,6 +52,7 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
 //        getHealthKitPermission()
     }
 
+    // Get rid of unnecessary stuff
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
 
@@ -66,10 +75,12 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
 //        }
 //    }
     
+    // When start button is tapped do these actions
     @IBAction func startTimer(sender: AnyObject) {
         timer = NSTimer.scheduledTimerWithTimeInterval(0.01, target: self, selector: Selector("updateTime"), userInfo: nil, repeats: true)
         zeroTime = NSDate.timeIntervalSinceReferenceDate()
         
+        // Set the initial values again so that distance doesn't jump up to wrong number
         distanceTraveled = 0.0
         startLocation = nil
         lastLocation = nil
@@ -78,26 +89,35 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.startUpdatingLocation()
     }
     
+    // Stop updating when stop button is pressed
     @IBAction func stopTimer(sender: AnyObject) {
         timer.invalidate()
         locationManager.stopUpdatingLocation()
     }
     
+    // The process of updating the timer
     func updateTime() {
         let currentTime = NSDate.timeIntervalSinceReferenceDate()
         var timePassed: NSTimeInterval = currentTime - zeroTime
+        
+        // Divide time passed by 60 (conversion factor from seconds to minutes) and subtract that value so as not to double-count this time
         let minutes = UInt8(timePassed / 60.0)
         timePassed -= (NSTimeInterval(minutes) * 60)
+        // Do the same conversion for seconds
         let seconds = UInt8(timePassed)
         timePassed -= NSTimeInterval(seconds)
+        // Convert remaining increase in time to milliseconds
         let millisecsX10 = UInt8(timePassed * 100)
         
+        // Format the values of each time componen
         let strMinutes = String(format: "%02d", minutes)
         let strSeconds = String(format: "%02d", seconds)
         let strMSX10 = String(format: "%02d", millisecsX10)
         
+        // Display the components (min, sec, millisec) as a stopwatch
         timerLabel.text = "\(strMinutes):\(strSeconds):\(strMSX10)"
         
+        // Kill the timer if it runs over the maximum value
         if timerLabel.text == "60:00:00" {
             timer.invalidate()
             locationManager.stopUpdatingLocation()
@@ -117,18 +137,21 @@ class TimerViewController: UIViewController, CLLocationManagerDelegate {
         
         
         if startLocation == nil {
-            //            Why set locations.first as CLLocation when in the beginning the variable type was specified?
+            // If the startLocation box is empty when locationManager function is called, fill it with the current location
             startLocation = currentLocation
         } else {
+            // If the function has already been called, calculate distance travelled
             let lastDistance = lastLocation.distanceFromLocation(currentLocation)
             distanceTraveled += lastDistance * 0.000621371
             
             //        Why format: "%.2f" and trimmedDistance?
             let trimmedDistance = String(format: "%.2f", distanceTraveled)
             
+            // Display the distance travelled
             milesLabel.text = "\(trimmedDistance) Miles"
         }
         
+        // Update the lastLocation value for next time loop runs
         lastLocation = currentLocation
     }
     
